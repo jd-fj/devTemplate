67fc0adaf71afbb667614a95a9768e0b16b26728
taylulz <taylulzcode@gmail.com>
2020-11-12 19:34:29 -0800

    install popper.js and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-import Pizza from './business.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Pizza from './business.js';
 import $ from 'jquery';
